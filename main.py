15a6cd45f331b78afd74815dfa867e12ad66d347
z k <zk@zettonoMacBook-Air.local>
2019-04-09 17:34:17 +0900

    first release.

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ def get_meta_info():
 
 
 if __name__ == "__main__":
-    # app.run(debug=True, host='0.0.0.0', port=5042, threaded=True)
-    from os import environ
-    app.run(debug=False, port=environ.get("PORT", 5000))
+    app.run()
+    # from os import environ
+    # app.run(debug=False, port=environ.get("PORT", 5000))
 
