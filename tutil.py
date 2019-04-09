2629c49d23a403d0aa4ce57c7b8fde597e578cc0
z k <zk@zettonoMacBook-Air.local>
2019-04-09 16:29:14 +0900

    clean up.

--- tutil.py
+++
@@ -1,9 +0,0 @@
-import os
-from datetime import timedelta, timezone, datetime
-
-JST = timezone(timedelta(hours=+9), 'JST')
-
-def get_last_modified(file_path):
-    st = os.stat(file_path).st_mtime
-    dt = datetime.fromtimestamp(st, JST)
-    return dt.strftime('%Y/%m/%d  %H:%M:%S')
